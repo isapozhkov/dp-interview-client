142b54af29094abd1b5d21a348c20da57abbbea0
i.sapozhkov <i.sapozhkov.93@gmail.com>
2020-01-23 22:05:40 +0300

    [frontend] Add axios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import axios from "axios";
 
+Vue.prototype.$http = axios.create({
+  baseURL: process.env.API_URL || "http://localhost:8081"
+});
 Vue.config.productionTip = false;
 
 new Vue({
