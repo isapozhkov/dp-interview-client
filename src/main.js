f9403cefce4d655467535cfacc693e7e7f1a93e6
i.sapozhkov <i.sapozhkov.93@gmail.com>
2020-01-24 20:30:14 +0300

    [frontend] Add vuetify. Change layout

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import axios from "axios";
+import vuetify from "./plugins/vuetify";
 
 Vue.prototype.$http = axios.create({
   baseURL: process.env.API_URL || "http://localhost:8081"
@@ -8,5 +9,6 @@ Vue.prototype.$http = axios.create({
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
